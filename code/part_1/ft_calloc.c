914f16e81af842268dc14069467b194a343f023a
andreyvdl <andreyvictordantasleal2016@gmail.com>
2024-04-13 00:30:07 -0300

    w+: finhesed some but need to complete isalpha

---
+++ code/part_1/ft_calloc.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/12 23:00:16 by adantas-          #+#    #+#             */
+/*   Updated: 2024/04/12 23:31:14 by adantas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	size_t	sum;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	sum = nmemb * size;
+	if (sum > INT32_MAX || sum > PTRDIFF_MAX || sum / nmemb != size)
+	{
+		errno = ENOMEM;
+		return (NULL);
+	}
+	ptr = malloc(sum);
+	if (ptr == NULL)
+		errno = ENOMEM;
+	return (ptr);
+}
--- headers/libft.h
+++ headers/libft.h
@@ -6,18 +6,25 @@
 /*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 19:22:22 by adantas-          #+#    #+#             */
-/*   Updated: 2024/04/12 22:32:43 by adantas-         ###   ########.fr       */
+/*   Updated: 2024/04/13 00:27:19 by adantas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <stdint.h>
+# include <errno.h>
 # include "t_string.h"
 
-int		ft_atoi(t_str nptr);
-int		ft_isdigit(int c);
 int		ft_isspace(int c);
+int		ft_atoi(t_str nptr);
 void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 
 #endif
